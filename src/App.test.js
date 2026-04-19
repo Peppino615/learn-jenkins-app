35d5995bfcf10b3a2706edb7bab7de90f7726d51
pan <da>
2026-04-19 17:27:52 +0100

    Junit report added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn Jenkins link', () => {
+test('Page title', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn Jenkins/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('Leran Jenkins.. link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Learn Jenkins on Udemy/i);
   expect(linkElement).toBeInTheDocument();
 });
